14c23c582e2324eec46b46789caaf350edd6101e
Your Name <dmitriirod@yandex.ru>
2023-02-23 23:16:10 +0300

    Added task08

---
+++ task08/Program.cs
@@ -0,0 +1,20 @@
+﻿// на вход число N
+// на выход все чётные числа от 1 до N
+Console.Write("Введите целое число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N > 2)
+{
+    Console.Write($"Четные числа от 1 до {N}: 2");
+    for(int i = 4; i <= N ; i = i + 2)
+    {
+        Console.Write(", " + i);
+}
+}
+else if (N == 2)
+{
+    Console.Write($"Четные числа от 1 до {N}: 2");
+}
+else
+{
+    Console.Write("Введите число больше 1");
+}
