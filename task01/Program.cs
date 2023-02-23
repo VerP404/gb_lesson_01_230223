a58ee380b1cc6659a325276abc2786afc6784336
Your Name <dmitriirod@yandex.ru>
2023-02-23 21:38:09 +0300

    Adden task01

---
+++ task01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите целое число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(b*b == a)
+{
+    Console.WriteLine($"Число {b} является квадратом числа {a}");
+}
+else
+{
+    Console.WriteLine($"Число {b} не является квадратом числа {a}");  
+}    
