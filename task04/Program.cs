98eb1df91eaf46a3309004c487aa7b6f889d6933
Your Name <dmitriirod@yandex.ru>
2023-02-23 22:53:16 +0300

    Added task04

---
+++ task04/Program.cs
@@ -0,0 +1,20 @@
+﻿// на вход 3 числа
+// на выход максимальное из них
+Console.Write("Введите целое число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (max < b)
+{
+    max = b;
+}
+if (max < c)
+{
+    max = c;
+}
+Console.WriteLine($"Число {max} максимальное из чисел: {a}, {b}, {c}");
