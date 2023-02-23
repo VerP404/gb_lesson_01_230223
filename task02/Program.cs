6e19fa5e70be552b03fa1b6a4e97e28c67757e52
Your Name <dmitriirod@yandex.ru>
2023-02-23 22:41:44 +0300

    Added task02

---
+++ task02/Program.cs
@@ -0,0 +1,19 @@
+﻿// на вход 2 числа
+// на выход какое число большее, а какое меньшее
+Console.Write("Введите целое число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b)
+{
+    Console.WriteLine($"Число {a} равно числу {b}");
+}
+else if(a > b)
+{
+    Console.WriteLine($"Число {a} больше числа {b}");
+}
+else
+{
+    Console.WriteLine($"Число {b} больше числа {a}");
+}    
